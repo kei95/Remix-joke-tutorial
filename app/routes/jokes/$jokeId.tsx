27e3c71e4ad00ac5fe1d656ac2ea107ce81bb4e7
kei95 <kei0613shine.star@gmail.com>
2022-05-30 20:41:32 -0700

    first commit - finish up until routing

---
+++ app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,12 @@
+export default function JokeRoute() {
+  return (
+    <div>
+      <p>Here's my hilarious joke:</p>
+      <p>
+        Why don't you find hippopotamuses hiding in trees? They're really good
+        at it.
+      </p>
+    </div>
+  );
+}
+// TODO: start from https://remix.run/docs/en/v1/tutorials/jokes#styling
