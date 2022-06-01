88e24bf3d8358e636c542a83879edd62a54fd56e
kei95 <kei0613shine.star@gmail.com>
2022-05-31 22:38:17 -0700

    add styles to the project

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,4 +1,11 @@
 import { Outlet } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+
+import stylesUrl from "~/styles/jokes.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
 
 export default function JakeRoute() {
   return (
